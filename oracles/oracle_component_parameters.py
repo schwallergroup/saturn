30b7b1fd83b824d183b5feaa8a68297dad6deaee
GuoJeff <jeff.guo@outlook.com>
2024-02-12 09:24:03 +0100

    removed Apache license and added preliminary_check flag to OracleComponent

--- oracles/oracle_component_parameters.py
+++ oracles/oracle_component_parameters.py
@@ -5,6 +5,11 @@ from oracles.reward_shaping.function_parameters import RewardShapingFunctionPara
 class OracleComponentParameters:
     name: str
     weight: float
-    # specific parameters contain oracle-specific parameters
+    # preliminary_check is a flag to indicate whether an OracleComponent should be executed first.
+    # If this OracleComponent is not satisfied, the SMILES is discarded. 
+    # The intended use case is for MPO objectives with expensive components, e.g., MD, 
+    # where the other objectives are cheap to compute and should be satisfied, e.g., MW < 500 Da.
+    preliminary_check: bool = False
+    # specific parameters contain oracle-specific parameters, e.g., reference SMILES for Tanimoto similarity
     specific_parameters: dict = {}
     reward_shaping_function_parameters: RewardShapingFunctionParameters
