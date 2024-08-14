a93cdb19ec1ee6c72f2792ff9a12e50ef80ec600
GuoJeff <jeff.guo@outlook.com>
2024-08-14 13:33:47 +0200

    chemistry filter during manual beam enumeration

--- dev/test_manual_beam_enumeration/manual_beam_enumeration.py
+++ dev/test_manual_beam_enumeration/manual_beam_enumeration.py
@@ -35,6 +35,11 @@ parser.add_argument(
     action="store_true",
     help="Extract large substructures."
 )
+parser.add_argument(
+    "--chemistry_filter",
+    action="store_true",
+    help="Apply chemistry filters."
+)
 
 args = parser.parse_args()
 
@@ -59,8 +64,41 @@ else:
     )
 beam_enumeration.pool_update(agent=checkpointed_model)
 
+# NOTE: Substructure extraction does not take into the raw counts 
+#       TODO: Test this later
+if args.chemistry_filter:
+    # Apply chemistry filters
+    #   - No unpaired electrons
+    #   - Largest ring <= 6
+
+    def size_largest_ring(smiles: str) -> int:
+        """
+        Returns the size of the largest ring in the molecule
+        """
+        mol = Chem.MolFromSmiles(smiles)
+        ring_info = mol.GetRingInfo()
+
+        return max([0] + [len(ring) for ring in ring_info.AtomRings()])
+
+    def has_unpaired_electrons(smiles: str) -> bool:
+        """
+        Returns whether any atom has unpaired electrons - catches radical and charged atoms
+        """
+        mol = Chem.MolFromSmiles(smiles)
+        return any(atom.GetNumRadicalElectrons() > 0 for atom in mol.GetAtoms())
+
+    entire_pool = beam_enumeration.entire_pool
+    pool = set()
+    for smiles in entire_pool.keys():
+        if size_largest_ring(smiles) <= 6 and not has_unpaired_electrons(smiles):
+            pool.add(entire_pool[smiles])
+            if len(pool) == 4:
+                break
+    smiles = pool
+else:
+    smiles = [Chem.MolToSmiles(mol) for mol in beam_enumeration.pool]
+
 # Save the substructures
-smiles = [Chem.MolToSmiles(mol) for mol in beam_enumeration.pool]
 with open(args.substructures_output_path, "w") as f:
-    for smile in smiles:
-        f.write(f"{smile}\n")
+    for s in smiles:
+        f.write(f"{s}\n")
