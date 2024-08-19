cd492ceb405c87c8d58aef08bb5ad591b3f4960d
GuoJeff <jeff.guo@outlook.com>
2024-08-19 16:59:33 +0200

    manual Beam Enumeration, added check for bicyclic rings and to filter out 3- and 4-membered rings

--- dev/test_manual_beam_enumeration/manual_beam_enumeration.py
+++ dev/test_manual_beam_enumeration/manual_beam_enumeration.py
@@ -31,6 +31,12 @@ parser.add_argument(
     help="Type of substructures to extract - 'structure' or 'scaffold'"
 )
 parser.add_argument(
+    "--size",
+    type=int,
+    required=True,
+    help="Minimum size of substructures to extract."
+)
+parser.add_argument(
     "--extract_large",
     action="store_true",
     help="Extract large substructures."
@@ -56,7 +62,7 @@ checkpointed_model = Generator.load_from_file(args.model_path, device="cuda")
 if args.extract_large:
     beam_enumeration = LargeBeamEnumeration(
         substructure_type=args.substructure_type,
-        substructure_min_size=25
+        substructure_min_size=args.size
     )
 else:
     beam_enumeration = BeamEnumeration(
@@ -64,21 +70,48 @@ else:
     )
 beam_enumeration.pool_update(agent=checkpointed_model)
 
-# NOTE: Substructure extraction does not take into the raw counts 
-#       TODO: Test this later
+# NOTE: Substructure extraction does not take into the raw frequency counts 
+#       TODO: Ablate this in the future
 if args.chemistry_filter:
-    # Apply chemistry filters
+    # Apply chemistry filters (inductive bias)
     #   - No unpaired electrons
-    #   - Largest ring <= 6
+    #   - Smallest ring size >= 5 and largest ring size <= 6 and no bicyclic rings
 
-    def size_largest_ring(smiles: str) -> int:
+    def ring_filter(smiles: str) -> bool:
         """
-        Returns the size of the largest ring in the molecule
+        Whether to keep the extracted substructure or not based on ring properties.
+        Returns True if:
+            1. There are no rings
+
+            or if and only if:
+
+            1. No bicyclic rings
+            2. Smallest ring size >= 5 and largest ring size <= 6
         """
         mol = Chem.MolFromSmiles(smiles)
         ring_info = mol.GetRingInfo()
 
-        return max([0] + [len(ring) for ring in ring_info.AtomRings()])
+        ring_sizes = [len(ring) for ring in ring_info.AtomRings()]
+
+        # If there are no rings, return True
+        if len(ring_sizes) == 0:
+            return True
+
+        # Check for bicylic rings
+        elif len(ring_sizes) > 1:
+            bicyclic_rule = Chem.MolFromSmarts("[R2]")
+            if mol.HasSubstructMatch(bicyclic_rule):
+                return False
+
+        # If the largest ring size is greater than 6, return False
+        if max(0, *ring_sizes) > 6:
+            return False
+        else:
+            # If there are 3- and 4-membered rings, return False
+            for size in [3, 4]:
+                if size in ring_sizes:
+                    return False
+            return True
 
     def has_unpaired_electrons(smiles: str) -> bool:
         """
@@ -90,7 +123,7 @@ if args.chemistry_filter:
     entire_pool = beam_enumeration.entire_pool
     pool = set()
     for smiles in entire_pool.keys():
-        if size_largest_ring(smiles) <= 6 and not has_unpaired_electrons(smiles):
+        if ring_filter(smiles) and not has_unpaired_electrons(smiles):
             # Flatten stereochemistry and then canonacalize
             processed_smiles = Chem.MolToSmiles(Chem.MolFromSmiles(smiles), isomericSmiles=False, canonical=True)
             pool.add(processed_smiles)
