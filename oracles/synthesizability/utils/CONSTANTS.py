194b60e19fd7febf42463ed6f84256d693922a09
GuoJeff <jeff.guo@outlook.com>
2024-09-12 12:13:23 +0200

    updated functional groups set

--- oracles/synthesizability/utils/CONSTANTS.py
+++ oracles/synthesizability/utils/CONSTANTS.py
@@ -1,24 +1,48 @@
+# Selected SMARTS from: https://www.daylight.com/dayhtml_tutorials/languages/smarts/smarts_examples.html
 FUNCTIONAL_GROUPS = {
-    "Alkene": "[CX3]=[CX3]",
-    "Alkyne": "[CX2]#C",
-    "Alcohol (Hydroxyl)": "[OX2H]",
-    "Ether": "[OD2]([#6])[#6]",
+    # Alkenes
+    "Allenic Carbon": "[$([CX2](=C)=C)]",
+    "Vinylic Carbon": "[$([CX3]=[CX3])]",
+    # Alkynes
+    "cetylenic Carbon": "[$([CX2]#C)]",
+    # Carbonyl-containing
+    "Ketone": "[CX3H1](=O)[#6]",
     "Aldehyde": "[CX3H1](=O)[#6]",
-    "Ketone": "[CX3](=O)[#6]",
     "Carboxylic Acid": "[CX3](=O)[OX2H1]",
-    "Ester": "[CX3](=O)[OX2][#6]",
+    "Acyl Halide": "[CX3](=[OX1])[F,Cl,Br,I]",
     "Amide": "[NX3][CX3](=[OX1])[#6]",
-    "Amine (Primary)": "[NX3;H2]",
-    "Amine (Secondary)": "[NX3;H1]([#6])[#6]",
-    "Amine (Tertiary)": "[NX3]([#6])([#6])[#6]",
-    "Nitrile": "[CX2]#N",
-    "Nitro Group": "[NX3](=O)[O-]",
-    "Thiols (Sulfhydryl)": "[SX2H]",
-    "Thioether": "[SX2]([#6])[#6]",
-    "Halides (Alkyl Halides)": "[F,Cl,Br,I]",
-    "Sulfoxide": "[#16X3+1]-[#8X1-]",
-    "Sulfone": "[SX4](=O)(=O)[#6]",
+    "Ester and Anhydride": "[#6][CX3](=O)[OX2H0][#6]",
+    "Carbamate": "[NX3,NX4+][CX3](=[OX1])[OX2,OX1-]",
+    # Ether
+    "Ether": "[OD2]([#6])[#6]",
     "Epoxide": "C1OC1",
-    "Anhydride": "[CX3](=O)O[CX3](=O)",
-    "Imine": "[CX3]=[NX3]"
+    # Amino-groups
+    "Primary Amine (and not Amide)": "[NX3;H2;!$(NC=[!#6]);!$(NC#[!#6])][#6]",
+    "Two Primary Amines or Secondary Amine": "[NX3;H2,H1;!$(NC=O)].[NX3;H2,H1;!$(NC=O)]",
+    # Imine
+    "Substituted or Unsubstituted Imine": "[$([CX3]([#6])[#6]),$([CX3H][#6])]=[$([NX2][#6]),$([NX2H])]",
+    # Imide
+    "Unsubstituted dicarboximide": "[CX3](=[OX1])[NX3H][CX3](=[OX1])",
+    "Substituted dicarboximide": "[CX3](=[OX1])[NX3H0]([#6])[CX3](=[OX1])",
+    "Dicarboximide": "[CX3](=[OX1])[NX3H0]([NX3H0]([CX3](=[OX1]))[CX3](=[OX1]))[CX3](=[OX1])",
+    # N-containing
+    "Nitrate": "[$([NX3](=[OX1])(=[OX1])O),$([NX3+]([OX1-])(=[OX1])O)]",
+    "Nitrile": "[NX1]#[CX2]",
+    "Nitro": "[$([NX3](=O)=O),$([NX3+](=O)[O-])][!#8]",
+    "Nitroso": "[NX2]=[OX1]",
+    # O-containing
+    "Hydroxyl": "[OX2H]",
+    "Hydroxyl in Alcohol": "[#6][OX2H]",
+    # S-containing
+    "Thiol": "[#16X2H]",
+    "Thioamide": "[NX3][CX3]=[SX1]",
+    "Carbo-Thiocarboxylate": "[S-][CX3](=S)[#6]",
+    "Carbo-Thioester": "S([#6])[CX3](=O)[#6]",
+    "Sulfide": "[#16X2H0]",
+    "Sulfone": "[$([#16X4](=[OX1])=[OX1]),$([#16X4+2]([OX1-])[OX1-])]",  # Permissive
+    "Sulfoxide": "[$([#16X3]=[OX1]),$([#16X3+][OX1-])]",  # Permissive
+    "Sulfate": "[$([#16X4](=[OX1])(=[OX1])([OX2H,OX1H0-])[OX2][#6]),$([#16X4+2]([OX1-])([OX1-])",
+    "Sulfamic Acid": "[$([#16X4]([NX3])(=[OX1])(=[OX1])[OX2H,OX1H0-]),$([#16X4+2]([NX3])([OX1-])([OX1-])[OX2H,OX1H0-])]",
+    # Halide
+    "Halide attached to Carbon": "[#6][F,Cl,Br,I]"
 }
