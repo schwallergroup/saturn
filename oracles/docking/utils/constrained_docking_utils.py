b6c56ac4c3eed049926ac2b563a9618c4ed69cd3
GuoJeff <jeff.guo@outlook.com>
2024-11-01 18:00:09 +0100

    saving top graphs now outputs parsed json. Will be used for GUI.

--- oracles/docking/utils/constrained_docking_utils.py
+++ oracles/docking/utils/constrained_docking_utils.py
@@ -100,34 +100,38 @@ def dense_reward_multiplier(
                 if interaction[3] >= interaction_criteria["distance_lower_bound"] and interaction[3] <= interaction_criteria["distance_upper_bound"]:
                     # Perfect reward
                     #distance_factor = 0.5
-                    distance_factor = 1.0
+                    #distance_factor = 1.0
+                    distance_factor = 0.75
                 elif interaction[3] < interaction_criteria["distance_lower_bound"]:
                     # Decrease reward by 0.1 for every 0.1 Angstrom deviation
                     deviation = interaction_criteria["distance_lower_bound"] - interaction[3]
                     #distance_factor = max(0, 0.5 - (deviation / 0.1 * 0.1))
-                    distance_factor = max(0, 1.0 - (deviation / 0.1 * 0.1))
+                    #distance_factor = max(0, 1.0 - (deviation / 0.1 * 0.1))
+                    distance_factor = max(0, 0.75 - (deviation / 0.1 * 0.1))
                 elif interaction[3] > interaction_criteria["distance_upper_bound"]:
                     # Decrease reward by 0.1 for every 0.1 Angstrom deviation
                     deviation = interaction[3] - interaction_criteria["distance_upper_bound"]
                     #distance_factor = max(0, 0.5 - (deviation / 0.1 * 0.1))
-                    distance_factor = max(0, 1.0 - (deviation / 0.1 * 0.1))
-
-                break
+                    #distance_factor = max(0, 1.0 - (deviation / 0.1 * 0.1))
+                    distance_factor = max(0, 0.75 - (deviation / 0.1 * 0.1))
             
                 # Check angle criterion
                 if float(distance_factor) == 0.0:
                     angle_factor = 0.0
                 elif interaction[4] >= interaction_criteria["angle_lower_bound"] and interaction[4] <= interaction_criteria["angle_upper_bound"]:
                     # Perfect reward
-                    angle_factor = 0.5
+                    #angle_factor = 0.5
+                    angle_factor = 0.25
                 elif interaction[4] < interaction_criteria["angle_lower_bound"]:
                     # Decrease reward by 0.1 for every 5 degrees deviation
                     deviation = interaction_criteria["angle_lower_bound"] - interaction[4]
-                    angle_factor = max(0, 0.5 - (deviation / 5 * 0.1))
+                    #angle_factor = max(0, 0.5 - (deviation / 5 * 0.1))
+                    angle_factor = max(0, 0.25 - (deviation / 5 * 0.1))
                 elif interaction[4] > interaction_criteria["angle_upper_bound"]:
                     # Decrease reward by 0.1 for every 5 degrees deviation
                     deviation = interaction[4] - interaction_criteria["angle_upper_bound"]
-                    angle_factor = max(0, 0.5 - (deviation / 5 * 0.1))
+                    #angle_factor = max(0, 0.5 - (deviation / 5 * 0.1))
+                    angle_factor = max(0, 0.25 - (deviation / 5 * 0.1))
 
                 # Break after finding the interaction for the residue
                 break
--- oracles/synthesizability/utils/extract_syntheseus_route_data.py
+++ oracles/synthesizability/utils/extract_syntheseus_route_data.py
@@ -3,7 +3,10 @@ import pickle
 import json
 import sys
 
-def extract_data(file_path: str, data_type: str) -> Dict[str, Union[str, int]]:
+def extract_data(
+    file_path: str,
+    data_type: str  
+) -> Dict[str, Union[str, int]]:
     # Load the pickle file
     with open(file_path, "rb") as f:
         route = pickle.load(f)  # set of syntheseus nodes
@@ -12,6 +15,8 @@ def extract_data(file_path: str, data_type: str) -> Dict[str, Union[str, int]]:
         data = extract_mol_data(route)
     elif data_type == "rxn":
         data = extract_rxn_data(route)
+    elif data_type == "all":
+        data = extract_all_data(route)
     else:
         raise ValueError(f"Invalid data type: {data_type}")
 
@@ -43,6 +48,41 @@ def extract_rxn_data(route: Set[Any]) -> Dict[str, Union[str, int]]:
 
     return syntheseus_route_data
 
+def extract_all_data(
+    route: Set[Any],
+) -> Dict[str, Union[str, int]]:
+    """
+    Extract both Mols and Reactions and relevant information.
+    The purpose is for saving the Syntheseus graphs of the top (by reward) generated molecules.
+
+    Extracted node information includes:
+    - depth
+    - is_mol
+    - mol_smiles
+    - is_rxn
+    - rxn_smiles
+    - rxn_class (dummy value added)
+    - rxn_name (dummy value added)
+    - is_purchasable
+
+    """
+    syntheseus_route_data = {}
+    # Sort nodes by depth
+    sorted_nodes = sorted(enumerate(route), key=lambda x: x[1].depth)
+    for idx, (_, node) in enumerate(sorted_nodes):
+        syntheseus_route_data[f"node_{idx+1}"] = {
+            "depth": node.depth,
+            "is_mol": hasattr(node, "mol"),
+            "mol_smiles": getattr(node, "mol", None).smiles if hasattr(node, "mol") else None,
+            "is_rxn": hasattr(node, "reaction"),
+            "rxn_smiles": str(getattr(node, "reaction", None)) if hasattr(node, "reaction") else None,
+            "rxn_class": None,  # Dummy value
+            "rxn_name": None,  # Dummy value
+            "is_purchasable": node.mol.metadata["is_purchasable"] if hasattr(node, "mol") else None
+        }
+
+    return syntheseus_route_data
+
 if __name__ == "__main__":
     pickle_file = sys.argv[1]  # The path to the pickle file is passed as an argument
     data_type = sys.argv[2]  # The type of data to extract is passed as an argument
@@ -51,3 +91,9 @@ if __name__ == "__main__":
         data_type=data_type
     )
     print(data)  # Capture result
+    #route = "/home/jeff/iclr_synth/saturn-dev/dev/synth/enforce/dump/syntheseus_results/output_0/mol_53/route_0.pkl"
+    #data = extract_data(
+    #    file_path=route,
+    #    data_type="all"
+    #)
+    #print(data)
