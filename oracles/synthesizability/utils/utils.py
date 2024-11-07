c25292e5d1aa7e2aa9d2134a5bc4f880fb66d24d
GuoJeff <jeff.guo@outlook.com>
2024-11-07 11:57:06 +0100

    style and small brevity change

--- oracles/synthesizability/utils/utils.py
+++ oracles/synthesizability/utils/utils.py
@@ -137,28 +137,28 @@ def fuzzy_matching_substructure(
     def _query_is_in_bbs(query_mol: str, 
                          enforced_blocks_mols: List[Mol]
     ) -> bool:
-        """Return True if query mol is in bbs
+        """
+        Return True if query mol is in enforced building blocks.
         """
         canon_query = Chem.MolToSmiles(query_mol)
 
-        # fix bug when score != 1 but the block is enforced
+        # Edge case when score != 1 but the block is enforced
         canonicalized_bbs_smiles = [Chem.MolToSmiles(mol) 
                                     for mol in enforced_blocks_mols]
         
-        is_in_bbs = any([(canon_query == smiles) 
-                         for smiles in canonicalized_bbs_smiles])
+        is_in_bbs = any([
+            (canon_query == smiles) for smiles in canonicalized_bbs_smiles
+        ])
         
         return True if is_in_bbs else False
-
 
     query_mol = Chem.MolFromSmiles(query_smiles)
     enforced_blocks_mols = [Chem.MolFromSmiles(smiles) for smiles in enforced_blocks_functional_groups.keys()]
     max_mcs_atoms = 0
 
-    # edge case if query mol is in bbs 
-    is_in_bbs = _query_is_in_bbs(query_mol, enforced_blocks_mols)
-
-    if is_in_bbs: return 1.0
+    # Edge case if query mol is in enforced building blocks
+    if _query_is_in_bbs(query_mol, enforced_blocks_mols):
+        return 1.0
 
     # FMS computation
     for block_mol in enforced_blocks_mols:
